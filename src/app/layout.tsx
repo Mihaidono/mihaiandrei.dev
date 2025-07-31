6de7737dd431f06b122c977b427abaca08f54bac
Mihaidono <amihai2001@gmail.com>
2025-07-31 13:28:01 +0300

    added analytics and forgot to add speed insight component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/navbar";
 import AnimatedBackground from "@/components/animated-background";
 import { Toaster } from "@/components/ui/sonner";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -30,6 +31,8 @@ export default function RootLayout({
         <Navbar />
         <main className="relative z-10">{children}</main>
         <Toaster position="top-center" />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
